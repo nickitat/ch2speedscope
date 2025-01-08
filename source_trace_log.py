a0c9710ea260ed5b0632afad8959043504e721f4
Nikita Taranov <nikita.taranov@clickhouse.com>
2025-01-08 21:14:29 +0100

    some strange changes

--- source_trace_log.py
+++ source_trace_log.py
@@ -23,23 +23,28 @@ class ClickHouseTraceLogAdaptor(IDataSourceAdaptor):
         )
         self.trace_types = trace_types
         self.sample_period = sample_period
+        self.query_ids = self._get_all_query_ids(query_id)
 
         self._check_revision()
         self._flush_logs()
+
+    def _get_all_query_ids(self, query_id):
+        query = f"SELECT groupUniqArray(query_id) FROM {self._table('system.query_log')} WHERE initial_query_id LIKE '{query_id}'"
+        return ast.literal_eval(self._run_query(query))
 
     def get_value_unit(self) -> str:
         return "milliseconds"
 
     def get_start_value(self) -> int:
-        query = f"SELECT {self._to_milliseconds('MIN(event_time_microseconds)')} FROM {self._table('system.trace_log')} WHERE query_id LIKE '{self.query_id}'"
+        query = f"SELECT {self._to_milliseconds('MIN(event_time_microseconds)')} FROM {self._table('system.trace_log')} WHERE query_id IN ({self.query_ids})"
         return ast.literal_eval(self._run_query(query))
 
     def get_end_value(self) -> int:
-        query = f"SELECT {self._to_milliseconds('MAX(event_time_microseconds)')} FROM {self._table('system.trace_log')} WHERE query_id LIKE '{self.query_id}'"
+        query = f"SELECT {self._to_milliseconds('MAX(event_time_microseconds)')} FROM {self._table('system.trace_log')} WHERE query_id IN ({self.query_ids})"
         return ast.literal_eval(self._run_query(query))
 
     def get_frames(self) -> List:
-        query = f"SELECT arrayReduce('groupUniqArray', arrayFlatten(groupUniqArray(trace))) FROM {self._table('system.trace_log')} WHERE query_id LIKE '{self.query_id}'"
+        query = f"SELECT arrayReduce('groupUniqArray', arrayFlatten(groupUniqArray(trace))) FROM {self._table('system.trace_log')} WHERE query_id IN ({self.query_ids})"
         return ast.literal_eval(self._run_query(query))
 
     def get_demangled_frames(self, frames) -> List[str]:
@@ -47,19 +52,17 @@ class ClickHouseTraceLogAdaptor(IDataSourceAdaptor):
         return ast.literal_eval(self._run_query(query))
 
     def get_threads(self) -> List[Tuple[int | str, int | str]]:
-        # first let's check if we have thread names
-        query = f"SELECT DISTINCT (thread_id, thread_name) AS id_to_name FROM {self._table('system.query_thread_log')} WHERE query_id LIKE '{self.query_id}' FORMAT JSONEachRow"
-        raw = [
-            ast.literal_eval(elem)["id_to_name"]
-            for elem in self._run_query(query).split("\n")[:-1]
-        ]
-        if len(raw):
-            return [(elem[0], elem[1]) for elem in raw]
-
-        # no luck, will use thread id as its name
-        query = f"SELECT arrayReduce('groupUniqArray', arrayFlatten(groupUniqArray(thread_ids))) FROM {self._table('system.query_log')} WHERE query_id LIKE '{self.query_id}'"
+        query = f"""
+        SELECT groupUniqArray((thread_id, ifNull(thread_name, 'noname')))
+          FROM (
+          SELECT thread_id
+            FROM {self._table('system.trace_log')}
+           WHERE query_id IN ({self.query_ids})
+          ) AS t1
+        LEFT JOIN system.query_thread_log AS t2 ON (t1.thread_id = t2.thread_id) AND (t2.query_id IN ({self.query_ids}))
+        """
         res = ast.literal_eval(self._run_query(query))
-        return [(elem, elem) for elem in res]
+        return res
 
     def get_events_for_thread(
         self, frames, thread_id, thread_name
@@ -67,7 +70,7 @@ class ClickHouseTraceLogAdaptor(IDataSourceAdaptor):
         query = f"""
           SELECT arrayMap(elem -> arrayFirstIndex(x -> x = elem, {frames}), trace) AS frames, {self._to_milliseconds('event_time_microseconds')} AS at
             FROM {self._table('system.trace_log')}
-           WHERE query_id LIKE '{self.query_id}' AND thread_id = {thread_id} AND trace_type IN {self.trace_types}
+           WHERE query_id IN ({self.query_ids}) AND thread_id = {thread_id} AND trace_type IN {self.trace_types}
         ORDER BY at
           FORMAT TSV
         """
@@ -89,15 +92,16 @@ class ClickHouseTraceLogAdaptor(IDataSourceAdaptor):
                CAST(if(mapContains(Settings, 'query_profiler_cpu_time_period_ns'), Settings['query_profiler_cpu_time_period_ns'], '100000000'), 'UInt64') AS cpu
         SELECT intDiv(if(real < cpu, real, cpu), 1000000)
           FROM {self._table('system.query_log')}
-         WHERE query_id LIKE '{self.query_id}' AND type = 'QueryStart'
+         WHERE query_id IN ({self.query_ids}) AND type = 'QueryStart'
+         LIMIT 1
         """
         return ast.literal_eval(self._run_query(query))
 
     def _check_revision(self):
         query_revision = self._run_query(f"""
-        SELECT revision
+        SELECT DISTINCT revision
           FROM {self._table('system.query_log')}
-         WHERE query_id LIKE '{self.query_id}' AND type = 'QueryStart'
+         WHERE query_id IN ({self.query_ids}) AND type = 'QueryStart'
         """)
         current_revision = self._run_query("SELECT revision()")
         assert query_revision == current_revision, "current ClickHouse revision is different from the revision at query time, demangled stacks will be wrong"
